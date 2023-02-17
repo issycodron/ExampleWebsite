cedf889ae706ae14e987b3e0d6f457bd2acc0a4e
Issy Codron <ic302@exeter.ac.uk>
2023-02-17 15:32:11 +0000

    updated hello route

--- api/main.py
+++ api/main.py
@@ -3,7 +3,15 @@ import datetime
 
 app = FastAPI()
 
-@app.get("/")
-def homepage():
+"""@app.get("/")
+async def homepage(): #async means server can run on multiple threads
     time = datetime.datetime.now()
-    return f"the time is {time}"
+    return f"the time is {time}"""
+
+@app.get("/hello") #add /hello at the end of url
+async def hello():
+    return f"Hello, world!"
+
+@app.get("/hello/{name}")
+async def hello_name(name: str):
+    return f"Hello, {name}"
