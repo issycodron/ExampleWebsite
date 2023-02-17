fede1c15b79899130d5aa01fd42abdde1c731fbf
Issy Codron <ic302@exeter.ac.uk>
2023-02-17 15:36:30 +0000

    added jinja2

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import datetime
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -8,9 +9,9 @@ async def homepage(): #async means server can run on multiple threads
     time = datetime.datetime.now()
     return f"the time is {time}"""
 
-@app.get("/hello") #add /hello at the end of url
+@app.get("/", response_class=HTMLResponse) #add /hello at the end of url
 async def hello():
-    return f"Hello, world!"
+    return f"<h1>Hello, world!</h1>"
 
 @app.get("/hello/{name}")
 async def hello_name(name: str):
