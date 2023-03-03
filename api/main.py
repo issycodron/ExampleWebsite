589ddf54cbfc37d72a869ca695431d9152c6369e
Issy Codron <ic302@exeter.ac.uk>
2023-03-03 14:00:55 +0000

    added name

--- api/main.py
+++ api/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import datetime
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 from . import settings
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 """@app.get("/")
 async def homepage(): #async means server can run on multiple threads
@@ -19,6 +30,14 @@ async def homepage(request: Request):
     return settings.TEMPLATES.TemplateResponse("homepage.html",{"request": request})
 
 
-@app.get("/hello/{name}")
-async def hello_name(name: str):
-    return f"Hello, {name}"
+@app.get("/hello/{name}") #app.post submits an entity to the specified resource
+async def hello_name(request: Request, name: str):
+    return settings.TEMPLATES.TemplateResponse("homepage.html",{"request": request, "name":name})
+
+
+class SampleInput(BaseModel):
+    x: int
+
+@app.post("/sample")
+async def sample(input: SampleInput):
+    return input.x**2
